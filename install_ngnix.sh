fa620f107f00437ea5332d7649c86976e4119207
ambikaiyer <ambikaiyer@administrators-MacBook-Pro.local>
2021-07-05 00:07:25 -0600

    Added user_data to install nginx

--- install_ngnix.sh
+++ install_ngnix.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# sleep until instance is ready
-until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
-  sleep 1
-done
-
 # install nginx
-sudo apt-get update
-sudo apt-get -y install nginx
+ apt-get update
+ apt-get -y install nginx
 
 # make sure nginx is started
-sudo service nginx start
+ service nginx start
 
 #Update index.html with static data
- echo "<h1>Cisco SPL</h1>" | sudo tee /var/www/html/index.html
+ echo "Cisco SPL"  > /var/www/html/index.html
 
 # Restart ngnix
-sudo service nginx restart
+service nginx restart
--- instance.tf
+++ instance.tf
@@ -4,20 +4,7 @@ resource "aws_instance" "web" {
   instance_type          = var.instance_type
   key_name               = aws_key_pair.generated_key.key_name
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  # user_data              = file("install_ngnix.tpl")
-
-  provisioner "file" {
-    source      = "install_ngnix.sh"
-    destination = "/tmp/install_ngnix.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/install_ngnix.sh",
-      "sudo sed -i -e 's/\r$//' /tmp/install_ngnix.sh", # Remove the spurious CR characters.
-      "sudo /tmp/script.sh",
-    ]
-  }
-
+  user_data              = file("install_ngnix.sh")
   tags = {
     Name = "webserver-${count.index + 1}"
   }
